28014409f94f1dd2f0293ffc38c7223dcc71e9ee
Julia Ayres <j.bobrineva@gmail.com>
2021-04-29 17:58:10 -0400

    passed tests for radix.js

--- radix.js
+++ radix.js
@@ -1,3 +1,31 @@
-function radixSort() {}
+function getDigit (num, idx) {
+	const foundNum = String(num)[String(num).length - 1 - idx];
+	return foundNum === undefined ? 0 : parseInt(foundNum);
+}
 
-module.exports = radixSort;
+function digitCount (arr) {
+	if (arr.length > 1) {
+		let res = Math.max(...arr).toString().length;
+		return parseInt(res);
+	} else {
+		let res = arr.toString().length;
+		return parseInt(res);
+	}
+}
+
+function radixSort (arr) {
+	let maxLength = digitCount(arr);
+
+	for (let i = 0; i < maxLength; i++) {
+		let buckets = Array.from({ length: 10 }, () => []);
+		for (let j = 0; j < arr.length; j++) {
+			let num = getDigit(arr[j], i);
+
+			if (num !== undefined) buckets[num].push(arr[j]);
+		}
+		arr = buckets.flat();
+	}
+	return arr;
+}
+
+module.exports = { radixSort, getDigit, digitCount };
