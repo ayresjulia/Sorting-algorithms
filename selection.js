c78e8bb03d738260883bdf10bf3ae62c5d19b181
Julia Ayres <j.bobrineva@gmail.com>
2021-04-29 15:44:43 -0400

    passed tests for selection.js

--- selection.js
+++ selection.js
@@ -1,3 +1,18 @@
-function selectionSort() {}
+function selectionSort (arr) {
+	for (let i = 0; i < arr.length; i++) {
+		let min = i;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[min] > arr[j]) {
+				min = j;
+			}
+		}
+		if (min !== i) {
+			let temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+		}
+	}
+	return arr;
+}
 
 module.exports = selectionSort;
